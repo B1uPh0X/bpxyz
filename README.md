88e8704dd2d4c5f71c9f85c995c2ba1c084aefca
B1uPh0X <B1uPh0x@protonmail.com>
2022-09-20 16:23:15 -0400

    Add font and colors
    Changes to be committed:
    	new file:   README.md
    	new file:   assets/fonts/linear-beam/1001fonts-linear-beam-eula.txt
    	new file:   assets/fonts/linear-beam/Linebeam.ttf
    	new file:   assets/fonts/linear-beam/linebeam.txt
    	modified:   five/script05.js

---
+++ README.md
@@ -0,0 +1,11 @@
+hehehe you read me uwu
+
+
+
+
+
+but dont read here, like plzzz dont 
+
+
+
+OOOOOWWWOOOOO AHHHHHHHHH nNOOOOO my SEENSSSITVE DATTATAAAAAAAAA!!!!!!
--- five/script05.js
+++ five/script05.js
@@ -1,13 +1,38 @@
+function preload() {
+  newfont = loadFont("..\\assets\\fonts\\linear-beam\\Linebeam.ttf");
+
+}
+
 function setup() {
     createCanvas(windowWidth-100, windowHeight-50);
   }
   function draw() {
-    setup();
+    textFont(newfont);
     background(0);
     fill(255);
-    textSize(96);
-    var oneX = (mouseX-(width/2))/4;
-    var oneY = (mouseY-(height/2))/4;
+    textSize(124);
+    textAlign(CENTER,CENTER);
+    
+    var oneX = (mouseX-(width/2))/8;
+    var oneY = (mouseY-(height/2))/8;
+    var twoX = (mouseX-(width/2))/4;
+    var twoY = (mouseY-(height/2))/4;
+    var theX = (mouseX-(width/2))/2;
+    var theY = (mouseY-(height/2))/2;
+
+    fill(0,255,0,128);
     text("B1uPh0X", (width/2)-oneX, (height/2)-oneY);
+    fill(0,255,255,128);
     text("B1uPh0X", (width/2)+oneX, (height/2)+oneY);
+
+    fill(255,255,0,128);
+    text("B1uPh0X", (width/2)-twoX, (height/2)-twoY);
+    fill(255,0,255,128);
+    text("B1uPh0X", (width/2)+twoX, (height/2)+twoY);
+
+    fill(255,0,0,128);
+    text("B1uPh0X", (width/2)-theX, (height/2)-theY);
+    fill(0,0,255,128);
+    text("B1uPh0X", (width/2)+theX, (height/2)+theY);
+
   }
